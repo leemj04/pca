83411fc828d29618ee8557fb9b165f9b33c75e69
jisung6723 <121074346+jisung6723@users.noreply.github.com>
2023-05-05 11:57:31 +0900

    Update buttonNewActivity.java

--- buttonNewActivity.java
+++ buttonNewActivity.java
@@ -1,6 +1,6 @@
 public class MainActivity extends AppCompatActivity {
     
-    View btn_name; // 연결할 버튼 이름
+    View btn_name; // 연결할 버튼 변수명
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+}
+
+//---------------------Fragment 일 경우--------------------------
+public class MainFragment extends Fragment {
+    //... 생략
+    View btn_name; // 연결할 버튼 변수명
+    
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    // ... 생략
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View temp = inflater.inflate(R.layout.fragment_name, container, false); // 현재 fragment의 이름, inflater는 하나만 있어도 됨
+        
+        btn_name = temp.findViewById(R.id.btn_id); // 연결할 버튼 아이디
+        btn_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), new_activity_name.class); // 연결할 새로운 액티비티 이름
+                startActivity(intent);
+            }
+        });
+        return temp;
     }
 }
