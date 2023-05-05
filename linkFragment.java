c76967faba5a59c9004802c535ee75194ca508b6
jisung6723 <121074346+jisung6723@users.noreply.github.com>
2023-05-05 12:07:26 +0900

    Create linkFragment.java

---
+++ linkFragment.java
@@ -0,0 +1,10 @@
+// Fragment를 불러오고 싶은 곳에 이 코드를 넣으세요
+FragmentManager fragmentManager = getFragmentManager();
+FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+fragmentTransaction.replace(R.id.container, fragment_name.newInstance("param1","param2")); 
+// container는 fragment를 집어넣을 layout id
+// frament_name은 불러올 fragment의 이름
+// param1, param2는 데이터를 넘겨줄 때 쓸 수 있다.
+
+fragmentTransaction.commit();
