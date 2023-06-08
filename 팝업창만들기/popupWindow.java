96f4c44a53c33b2930f44857e9d36aa8a40fb1a0
jisung6723 <121074346+jisung6723@users.noreply.github.com>
2023-06-08 23:36:39 +0900

    Create popupWindow.java

---
+++ 팝업창만들기/popupWindow.java
@@ -0,0 +1,22 @@
+LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+// main_layout이 팝업창을 띄울 화면(배경)
+View popupView = inflater.inflate(R.layout.main_layout, null);
+
+// ConstraintLayout 부분은 어떤 Layout인지 따라 달라짐, LinearLayout이면 LinearLayout.~~ 이런식
+int width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+int height = ConstraintLayout.LayoutParams.MATCH_PARENT;
+//팝업창 바깥을 클릭했을 때 팝업 종료하기 기능, false면 꺼짐
+boolean focusable = true; 
+final PopupWindow popupWindow = new PopupWindow(popupView,width, height, focusable);
+
+// AnyViewLayout은 현재 메인 Layout에 있는 View중 아무거나 가져오면 됨, 토큰용
+popupWindow.showAtLocation(AnyViewinLayout, Gravity.CENTER, 0, 0);
+
+Button btn = (Button) popupView.findViewById(R.id.close_btn);
+btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        popupWindow.dismiss(); // 팝업창 닫는 함수
+    }
+});
